aa7d931bb54ec9dc2dd33615123ca580f4ff2195
fillsion <60821971+fillsion@users.noreply.github.com>
2021-11-04 10:05:11 -0500

    Delete dist directory

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-_app["default"].listen(3000);
-
-console.log("Server on port 3000");
